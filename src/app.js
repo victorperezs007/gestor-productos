a237a88511e7a45be229c579ef41ce2917db50f9
Maicxl09 <rendongonzalezmaicol2@gmail.com>
2025-11-14 08:55:33 -0500

    Maicol crear rutas y config express

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+import productRoutes from "../routes/productRoutes";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/productos",productRoutes);
+
+export default app;
